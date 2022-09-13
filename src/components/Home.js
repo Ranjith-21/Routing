666b7dd3509a293e8d66526e745cccf9c036d091
Ranjith-21 <104754373+Ranjith-21@users.noreply.github.com>
2022-09-13 13:29:07 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
         <h1>HOME</h1>
-        <img src='https://cdn.pixabay.com/photo/2015/09/09/19/56/office-932926_960_720.jpg' width="700px"/>
+        <img src='https://cdn.pixabay.com/photo/2015/09/09/19/56/office-932926_960_720.jpg' alt='' width="700px"/>
     </div>
   )
 }
